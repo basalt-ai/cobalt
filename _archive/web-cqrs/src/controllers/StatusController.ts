9f10fcba7ea62b92768e26a7b6552c783ba25c53
Guillaume <guillaume@solidy.me>
2026-02-09 15:57:30 -0800

    remove archives

--- _archive/web-cqrs/src/controllers/StatusController.ts
+++
@@ -1,9 +0,0 @@
-import { GetStatusQuery } from '@/queries/GetStatusQuery';
-import type { StatusResponse } from '@cobalt/types';
-
-export class StatusController {
-	static async getStatus(): Promise<StatusResponse> {
-		const query = new GetStatusQuery();
-		return await query.execute();
-	}
-}
